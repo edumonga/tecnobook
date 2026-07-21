a6b00c6a30f21c04fa41685b768fe10f59a52994
Eduardo Santana <accedu77@gmail.com>
2026-07-21 08:56:55 -0300

    inicio do carrosel
    mt bug

---
+++ script/carrosel.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
